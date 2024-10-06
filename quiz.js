33c9332284cc8422a1e8a169e6cf63bd63c3b241
Nasr <nasr.asri10@gmail.com>
2024-10-06 14:35:31 +0100

    update HTML & CSS files and add quiz.js

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const checkedBtn = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = checkedBtn.value;
+
+    if(userAnswer == correctAnswer) {
+        document.querySelector('#feedback').textContent = "Correct! Well done.";
+    } else {
+        document.querySelector('#feedback').textContent = "That's incorrect. Try again!";
+    }
+}
